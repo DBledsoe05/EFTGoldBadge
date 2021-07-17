890be8b15956e5c3980d81123fc355c66bfc52f6
Denzel <dbledsoe374@gmail.com>
2021-07-17 00:57:37 -0400

    Had a few typos did a quick fix

--- EFT_GoldBadge_Applications/Cafe_Repo_Test.cs
+++ EFT_GoldBadge_Applications/Cafe_Repo_Test.cs
@@ -1,14 +1,41 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using MenuItems;
+using MenuItems.Cafe_Enums;
+using MenuItems_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace EFT_GoldBadge_Applications
 {
     [TestClass]
-    public class UnitTest1
+    public class Cafe_Repo_Test
     {
-        [TestMethod]
-        public void TestMethod1()
+        private readonly MealsRepo _repo = new MealsRepo();
+
+        [TestInitialize]
+        public void Arrang()
         {
+            Meals meals = new Meals(1, "Big Monster", "Only Roast Beef for the hungry out there.", Ingrediant.RoastBeef, 5);
+            _repo.AddMealToList(meals);
+        }
+
+        [TestMethod]
+        public void RemoveMealsItem_MealDoesNotExist_ReturnsFalse()
+        {
+            string mealGone = "Gone";
+
+            bool result = _repo.RemoveMealsItems(mealGone);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void RemoveMealsItem_MealDoesExist_ReturnsTrue()
+        {
+            string mealGone = "Gone";
+
+            bool result = _repo.RemoveMealsItems(mealGone);
+
+            Assert.IsFalse(result);
         }
     }
 }
--- Komodo_Claims_Test/Claims_Repo_Test.cs
+++ Komodo_Claims_Test/Claims_Repo_Test.cs
@@ -1,14 +1,42 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Claim_Poco;
+using Claim_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace Komodo_Claims_Test
 {
     [TestClass]
-    public class UnitTest1
+    public class ClaimsRepoTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private readonly ClaimRepo _claimsRepo = new ClaimRepo();
+
+        [TestInitialize]
+        public void Arrange()
         {
+            Claims claims = new Claims(23132, "House", "Tree fall on house.", 3131, new DateTime (10/12/2021), new DateTime (10/15/2021), true);
+            _claimsRepo.AddClaimsToList(claims);
+        }
+        [TestMethod]
+        public void UpdateClaims_ClaimDoesNotExist_ReturnsFalse()
+        {
+            string claimType = "Car";
+            Claims updatedClaims = new Claims();
+
+            bool result = _claimsRepo.UpdateExistingClaims(claimType, updatedClaims);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Delete_MovieDoesNot_Exist()
+        {
+            string deleteClaim = "delete";
+
+            bool result = _claimsRepo.RemoveClaimsFromList(deleteClaim);
+            Claims claims = _claimsRepo.GetClaimsByClaimType(deleteClaim);
+
+            Assert.IsNull(claims);
+            Assert.IsFalse(result);
         }
     }
 }
